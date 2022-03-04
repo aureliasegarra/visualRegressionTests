8778374370bffd664c1680c7722c03d0b6f2524e
Aurelia Segarra <av.segarra@gmail.com>
2022-03-04 14:50:11 +0100

    :construction: WIP: Visual Regression Test on different screen sizes

--- cypress/integration/saucefile.js
+++ cypress/integration/saucefile.js
@@ -1,12 +1,16 @@
 /// <reference types= "Cypress" />
 
-describe('Testing landing page', () => {
-    beforeEach(() => {
-        cy.viewport('macbook-15');
-        cy.visit('https://www.saucedemo.com/');
-    });
+['macbook-15', 'ipad-2', 'iphone-x'].forEach(screen => {
 
-    it('The logo should display correctly', () => {
-        cy.get('.login_logo').matchImageSnapshot("Logo");
-    });
- })
+    describe('Testing landing page', () => {
+        beforeEach(() => {
+            cy.viewport(screen);
+            cy.visit('https://www.saucedemo.com/');
+        });
+    
+        it('The logo should display correctly', () => {
+            cy.matchImageSnapshot(`Image on ${screen}`);
+        });
+     });
+})
+
